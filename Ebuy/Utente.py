fd6a8919edfc1e63b61b649016925594cc5e9f6b
malaonda222 <bandinelli.lisa@gmail.com>
2025-07-12 17:26:17 +0200

    new commit

--- Ebuy/Utente.py
+++ Ebuy/Utente.py
@@ -14,8 +14,10 @@ class Utente(ABC):
     def __init__(self, *, username: str, data_registrazione: datetime):
         self._username = username
         self._data_registrazione = data_registrazione
+    
     def username(self) -> str:
         return self._username
+    
     def data_registrazione(self) -> datetime:
         return self._data_registrazione
 
@@ -26,8 +28,10 @@ class VenditoreProfessionale(Utente):
     def __init__(self, *, username: str, data_registrazione: datetime, vetrina: Url|None = None):
         super().__init__(username=username, data_registrazione=data_registrazione)
         self.set_vetrina(vetrina)
+    
     def vetrina(self) -> Url|None:
         return self._vetrina
+    
     def set_vetrina(self, vetrina: Url|None) -> None:
         try:
             if self._vetrina:
