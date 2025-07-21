c6475d9359a63cede541cb2d1749ad4acb3a8767
malaonda222 <bandinelli.lisa@gmail.com>
2025-07-21 11:57:35 +0200

    commit

---
+++ DESIGN/Ebuy/CodiceProf/Bid.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+from customtypes import *
+from bid_ut import bid_ut 
+from asta_bid import asta_bid
+from OggettoDelPost import Asta 
+from Utente import UtentePrivato 
+
+import datetime
+
+class Bid:
+    _istante: datetime                 # imm
+    _asta_bid_link: asta_bid._link    # imm, certamente noto alla nascita
+    _bid_ut_link: bid_ut._link        # imm, certamente noto alla nascita
+    
+    def __init__(self, *, asta: Asta, up: UtentePrivato) -> None:
+        self._istante = datetime.datetime.now()
+        self._add_link_asta_bid(asta)
+        self._add_link_bid_ut(up)
+        
+    def istante(self) -> datetime:
+        return self._istante
+    
+    def _add_link_asta_bid(self, asta: Asta) -> None:
+        l = asta_bid._link(asta, self)
+        self._asta_bid_link = l
+        asta._add_asta_bid(l)
+        
+    def _add_link_bid_ut(self, up: UtentePrivato) -> None:
+        l = bid_ut._link(self, up)
+        self._bid_ut_link = l
+        up._add_bid_ut_link(l)
+        
+    def __repr__(self) -> str:
+        return f"Bid(istante={self.istante()},\n \
+                utente_privato={self._bid_ut_link.utente_privato()},\n \
+                asta={self._asta_bid_link.asta()})"
